be9407ccc04dae36902bd8b7264c4e8e3fea1f43
Antira <antira@builk.com>
2017-11-23 01:41:11 +0700

    edit style oncall schedule page

--- src/app/oncall-schedule/oncall-schedule.component.ts
+++ src/app/oncall-schedule/oncall-schedule.component.ts
@@ -20,13 +20,13 @@ export class OncallScheduleComponent implements OnInit {
     getProfileImageURL(lanId):string{
     lanId = "ap_kkhumth"
     let imageURL = './assets/img/user1.png'
-      if (lanId){
-        // imageURL = 'https://mysite.na.xom.com/User%20Photos/Profile%20Pictures/ap_kkhumth_LThumb.jpg'
-         imageURL =  'https://mysite.na.xom.com/User%20Photos/Profile%20Pictures/' + (lanId.replace('\\', '_')) + '_LThumb.jpg';
-      }
-      else{
-    imageURL = "https://vignette.wikia.nocookie.net/doraemon/images/c/c0/Doraemon_%282002%29.png/revision/latest/scale-to-width-down/350?cb=20170327161129&path-prefix=en"
-      }
+    //   if (lanId){
+    //     // imageURL = 'https://mysite.na.xom.com/User%20Photos/Profile%20Pictures/ap_kkhumth_LThumb.jpg'
+    //      imageURL =  'https://mysite.na.xom.com/User%20Photos/Profile%20Pictures/' + (lanId.replace('\\', '_')) + '_LThumb.jpg';
+    //   }
+    //   else{
+    // imageURL = "https://vignette.wikia.nocookie.net/doraemon/images/c/c0/Doraemon_%282002%29.png/revision/latest/scale-to-width-down/350?cb=20170327161129&path-prefix=en"
+    //   }
     return imageURL
   }
 
