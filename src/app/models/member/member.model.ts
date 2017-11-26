a32b59691cfcf078af2fcc8576313820989eb29a
Antira <antira@builk.com>
2017-11-26 14:02:36 +0700

    ----- add EditUserModel

---
+++ src/app/models/member/member.model.ts
@@ -0,0 +1,12 @@
+export class EditUserModel {
+    public Domain : string;
+    public Username : string;
+    public PersonalEmail : string;
+    public SharedEmail : string;
+    public IsGroupAdministrator : boolean;
+    public IsEscalationReceiver : boolean;
+    public IsWorkHourReceiver : boolean;
+    public IsAcknowledgeResultReceiver : boolean;
+}
+
+
