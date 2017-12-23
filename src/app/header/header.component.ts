b4f12ce11f21ebb3f83be96498dd7cf22f655bd5
Antira <antira@builk.com>
2017-12-23 21:49:54 +0700

    #Edit Header NavBar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery'; 
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
+
+    $(function () { // DOM ready 
+      // If a link has a dropdown, add sub menu toggle. 
+      $('#navbarResponsive nav ul li a:not(:only-child)').click(function (e) { 
+        $(this).siblings('#navbarResponsive .nav-dropdown').toggle(); 
+        // Close one dropdown when selecting another 
+        $('#navbarResponsive .nav-dropdown').not($(this).siblings()).hide(); 
+        e.stopPropagation(); 
+      }); 
+      // Clicking away from dropdown will remove the dropdown class 
+      $('html').click(function () { 
+        $('#navbarResponsive .nav-dropdown').hide(); 
+      }); 
+      // Toggle open and close nav styles on click 
+      $('#nav-toggle').click(function () { 
+        $('nav ul').slideToggle(); 
+      }); 
+ 
+ 
+      $('#nav-toggle-link-1').click(function () { 
+        $('nav ul').slideToggle(); 
+      }); 
+ 
+      $('#nav-toggle-link-2').click(function () { 
+        $('nav ul').slideToggle(); 
+      }); 
+ 
+      $('#nav-toggle-link-3').click(function () { 
+        $('nav ul').slideToggle(); 
+      }); 
+ 
+      $('#nav-toggle-link-4').click(function () { 
+        $('nav ul').slideToggle(); 
+      }); 
+ 
+      $('#nav-toggle-link-5').click(function () { 
+        $('nav ul').slideToggle(); 
+      }); 
+ 
+      $('#nav-toggle-link-6').click(function () { 
+        $('nav ul').slideToggle(); 
+      }); 
+ 
+      // Hamburger to X toggle 
+      // $('#nav-toggle').on('click', function () { 
+      //   this.classList.toggle('active'); 
+      // }); 
+ 
+      
+ 
+    }); // end DOM ready 
+
   }
 
 }
